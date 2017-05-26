1098fa82833203e35aa21af669123400e02c394d
xuleyuan <ly.xu@boyuanitsm.com>
2017-05-26 11:36:00 +0800

    addMessage
    ShareSDK/SMSSDK use SWIFT

---
+++ SMSSDK-Bridge-Header.h
@@ -0,0 +1,14 @@
+//
+//  SMSSDK-Bridge-Header.h
+//  SWC
+//
+//  Created by Xuleyuan on 2017/5/26.
+//  Copyright © 2017年 hrscy. All rights reserved.
+//
+
+#ifndef SMSSDK_Bridge_Header_h
+#define SMSSDK_Bridge_Header_h
+
+#import <SMS_SDK/SMSSDK.h>
+
+#endif /* SMSSDK_Bridge_Header_h */
